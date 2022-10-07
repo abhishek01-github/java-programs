f0126cca64c8f4fc98376c25d7fdb69ffd98514e
Abhishek Pandey <abhishekpandey118852@gmail.com>
2022-10-07 19:51:33 +0530

    programs commited

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter value of a and b");
+        a=scn.nextInt();
+        b=scn.nextInt();
+        int sum = a+b;
+        System.out.println("Sum of the numbers "+a+" and "+b+" is: "+sum);
+    }
+}
