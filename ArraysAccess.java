f36b485df2adab4393d0899d4b6f49d52f6ed0bd
Abhishek Pandey <abhishekpandey118852@gmail.com>
2023-03-09 19:51:21 +0530

    committed

---
+++ ArraysAccess.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] arr = {23,534,34,42,54};
+        // using for each loop
+        for(int num : arr)
+            System.out.println(num + " ");
+        // using to string method
+        System.out.println(Arrays.toString(arr));
+    }
+}
